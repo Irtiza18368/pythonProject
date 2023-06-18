f9f77be4c78bc3b9cb9457a66f0ef146ccdadf9b
ec221166 <ec221166@qmul.ac.uk>
2023-06-18 20:21:53 +0100

    Revert "Update of Tic tac toe"
    This reverts commit cef00d77331e48f1f53f3efd7de2c0bc76323ecf.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import messagebox
 
 # Create the main window
 root = tk.Tk()
@@ -10,84 +9,45 @@ board = [[' ', ' ', ' '],
          [' ', ' ', ' '],
          [' ', ' ', ' ']]
 
-current_player = 'X'
-
-
-# Function for handling player input
-
-def player_moves(row1, column1, button1):
-    if board[row1][column1] == ' ':
-        board[row1][column1] = current_player
-        button1.config(text=current_player, state='disabled', relief='sunken')
-        if check_win(current_player):
-            messagebox.showinfo("Game Over", f" {current_player} wins!")
-            reset_game()
-        elif check_tie():
-            messagebox.showinfo("Game Over! It's a tie!")
-            reset_game()
-        else:
-            switch_player()
-
 
 # Update the game board in the GUI
 def display_board():
     for i in range(3):
         for j in range(3):
-            cell_label = tk.Label(root, text=board[i][j], font=('Arial', 20), width=8, height=4,
-                                  relief='solid')
+            cell_label = tk.Label(root, text=board[i][j], font=('Arial', 14),
+                                  width=8, height=4, relief='solid')
             cell_label.grid(row=i, column=j)
 
 
-def check_win(player):
-    for row in range(3):
-        if board[row][0] == board[row][1] == board[row][2] != ' ':
-            return True
-
-    for column in range(3):
-        if board[0][column] == board[1][column] == board[2][column] != ' ':
-            return True
-
-    if board[0][0] == board[1][1] == board[2][2] != ' ':
-        return True
-
-    if board[0][2] == board[1][1] == board[2][0] != ' ':
-        return True
-    return False
-
-
-def check_tie():
-    for row1 in range(3):
-        for column1 in range(3):
-            if board[row1][column1] == ' ':
-                return False
-        return True
-
-
-def reset_game():
-    for row in range(3):
-        for column in range(3):
-            board[row][column] = ' '
-        for button in button_list:
-            button.config(text=' ', state='normal', relief='raised')
-
-
-def switch_player():
-    global current_player
-    current_player = 'O' if current_player == 'X' else 'X'
-
-
-button_list = []
-
-# Creating GUI Buttons
-
-for row in range(3):
-    for column in range(3):
-        button = tk.Button(root, text=' ', font=('Arial', 20), width=8, height=4, relief='raised')
-        button.grid(row=row, column=column)
-        button.config(command=lambda r=row, c=column, b=button: player_moves(r, c, b))
-        button_list.append(button)
-
+# Test the display_board() function
+display_board()
 
 # Start the GUI main loop
-
 root.mainloop()
+
+
+# Function to handle player input
+def player_input():
+    while True:
+        try:
+            row = int(input("Enter the row(0,1, or 2):"))
+            column = int(input("Enter the row(0,1, or 2):"))
+            if row in range(3) and column in range(3) and board[row][column] == ' ':
+                return row, column
+            else:
+                print("Invalid input!")
+        except ValueError:
+            print("Invalid input")
+
+
+# Example
+row1, column1 = player_input()
+print("Player entered row:", row1, "column:", column1)
+
+
+# Function for updating the game board
+def update_board(row2, column2, player):
+    board[row2][column2] = player
+
+row = 1
+column = 2
